facd3f142ac0b5c2b38bb150e5048aa699c2bc40
Theo Weller <weller.theodor@gmail.com>
2019-04-22 21:09:05 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Code your solutions in this file
 const myBag = ["Hella papes", "Supreme Brick", "Live mice"];
 
-for(let x = 0; x < myBag.length; x++;) {
+for(let x = 0; myBag.length < x; x++;) {
   console.log(x)
 }
