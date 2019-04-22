5b05be0d897e909354bad6d9c733bebb04460fc8
Theo Weller <weller.theodor@gmail.com>
2019-04-22 21:13:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,3 +4,9 @@ const myBag = ["Hella papes", "Supreme Brick", "Live mice"];
 for(let x = 0; myBag.length > x; x++) {
   console.log(myBag[x])
 }
+
+let countdown = 20;
+
+while (countdown > 0) {
+  console.log(countdown--)
+}
